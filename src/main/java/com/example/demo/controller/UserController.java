cf542e9608f720a6eac8b0d668d8f75d87745b57
EdytaToporowicz <edyta.toporowicz@gmail.com>
2021-03-13 21:04:34 +0100

    category-list - poprawki

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -2,10 +2,8 @@ package com.example.demo.controller;
 
 
 import com.example.demo.model.UserDto;
-
 import com.example.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
